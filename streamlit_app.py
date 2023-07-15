d7467c18cac4fbfa361a55519d5d5a07f91bb83f
hrmanjusnowflake <139643568+hrmanjusnowflake@users.noreply.github.com>
2023-07-15 22:53:43 +0530

    Update streamlit_app.py
    Added pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title('my light is here') 
 streamlit.header('it''s a headed') 
 streamlit.text('it''s a text') 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
